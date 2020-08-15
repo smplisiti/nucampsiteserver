029da7ccf4abe8106dbb8164e6cd0d748a6c9fea
Heather <smplisiti@gmail.com>
2020-08-14 21:30:23 -1000

    not workiing right now

--- models/promotion.js
+++ models/promotion.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+
 const promotionSchema = new Schema(
   {
     name: {
